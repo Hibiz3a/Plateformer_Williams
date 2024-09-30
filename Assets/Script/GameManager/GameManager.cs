de9d3905548bb9299e258dfdcc70141911975316
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2024-09-30 16:26:10 +0200

    End Game, NextLevel

--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private float Score = 0f;
     private float Timer = 0f;
+
     public float _score
     {
         get { return Score; }
     }
+
     public float _timer
     {
         get { return Timer; }
     }
+
     public static GameManager Instance;
+
+    private int CurrentScene;
 
     void Start()
     {
@@ -40,7 +46,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("Your Dead...");
         }
     }
-    
+
     public void IncreaseScore(float _score)
     {
         Score += _score;
@@ -50,6 +56,19 @@ public class GameManager : MonoBehaviour
     {
         Score -= _score;
     }
-    
-    
+
+    public void NextLevel()
+    {
+        CurrentScene++;
+        SceneManager.LoadScene(CurrentScene);
+        End();
+    }
+
+    private void End()
+    {
+        if (CurrentScene == 15)
+        {
+            Application.Quit();
+        }
+    }
 }
