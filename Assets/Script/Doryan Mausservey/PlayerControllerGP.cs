1f3ada11e1c397c2cba35ab168884c98807d8b28
wighthood <zefires@gmail.com>
2024-10-02 17:31:49 +0200

    added jump need to groundcheck

--- Assets/Script/Doryan Mausservey/PlayerControllerGP.cs
+++ Assets/Script/Doryan Mausservey/PlayerControllerGP.cs
@@ -1,9 +1,12 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerControllerGP : MonoBehaviour
 {
     [SerializeField] private float Speed;
+    [SerializeField] private float JumpForce;
     [SerializeField] private Rigidbody2D RB2D;
+    [SerializeField] private bool GroundCheck;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,11 @@ public class PlayerControllerGP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float movehorizontal = Input.GetAxis("Horizontal");
-        Vector2 Movement = new Vector2(movehorizontal,RB2D.velocity.y);
-        RB2D.AddForce(Movement);
+        //if(transform.GetComponentInChildren<BoxCollider2D>() == true) {
+        RB2D.position += new Vector2(Input.GetAxis("Horizontal")*Time.deltaTime*Speed,0);
+        if (GroundCheck == true ) 
+        {
+                RB2D.position += new Vector2(0, Input.GetAxis("Jump") * Time.deltaTime * JumpForce);
+        }
     }
 }
