67ed4d107827fe2dde6ae3691f1c4a494c514f71
lucasdprx <150352145+lucasdprx@users.noreply.github.com>
2024-10-03 10:05:39 +0200

    Fix jump

--- Assets/Script/Lucas Darpeix/DW_Jump_System.cs
+++ Assets/Script/Lucas Darpeix/DW_Jump_System.cs
@@ -1,16 +1,16 @@
+using UnityEditor;
 using UnityEngine;
 
 public class DW_Jump_Systeme : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    private int _jumpNumber = 4;
+    private int _jumpNumber = 3;
     [SerializeField] private float _jumpForce;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _jumpNumber > 0)
@@ -18,5 +18,9 @@ public class DW_Jump_Systeme : MonoBehaviour
             _rb.velocity += Vector2.up * _jumpForce;
             _jumpNumber--;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _jumpNumber = 3;
     }
 }
