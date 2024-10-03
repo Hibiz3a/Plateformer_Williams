838ed5d4f26bfc8e7764846b58636304e35c381f
MisterHF <h.florianpro@gmail.com>
2024-10-03 13:16:31 +0200

    Add movement player

--- Assets/Script/Florian/HF_PlayerController.cs
+++ Assets/Script/Florian/HF_PlayerController.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HF_PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2 Dir;
+    private float Horizontal;
+    private float Vertical;
+    [SerializeField] private float Speed;
+
+    [SerializeField] Rigidbody2D Rb;
+    [SerializeField] Transform Transform;
+
+    private void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z))
+        {
+            Dir = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            Dir = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q))
+        {
+            Dir = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            Dir = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Rb.velocity = Dir * Speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("HF_HitWall"))
+        {
+
+
+            Debug.Log("!!!!");
+        }
     }
 }
