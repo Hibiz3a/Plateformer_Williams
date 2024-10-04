a90cc4b8af539699044bea9e1989fdc199b614a5
xinom4966 <sd.dessay@gmail.com>
2024-10-04 15:10:03 +0200

    Réparation de bug
    - Les boutons ont été modifiés pour régler leurs problèmes
    - Le joueur peut désormais quand même sauter sur le coin d'une plateforme

--- Assets/Script/Simon/SD_ButtonBehaviour.cs
+++ Assets/Script/Simon/SD_ButtonBehaviour.cs
@@ -2,22 +2,32 @@ using UnityEngine;
 
 public class SD_ButtonBehaviour : MonoBehaviour
 {
-    [SerializeField] private Transform MyTransform;
+    [SerializeField] private SpriteRenderer ButtonColor;
     [SerializeField] private SD_DoorBehaviour ConnectedDoor;
-    private Vector3 PressedScale = new Vector3(0, -0.15f, 0);
-    private Vector3 PressedPosition = new Vector3(0, -0.15f, 0);
+    private Color BaseColor;
+    private Color PressedColor = new Color(0.5f, 0.5f, 0.5f, 1);
+    private int PresserCount = 0;
+
+    private void Start()
+    {
+        BaseColor = ButtonColor.color;
+    }
     private void OnCollisionEnter2D(Collision2D _collision)
     {
-        MyTransform.position += PressedPosition;
-        MyTransform.localScale += PressedScale;
-        _collision.gameObject.transform.position += PressedPosition*2;
-        ConnectedDoor.OpenDoor();
+        PresserCount++;
+        ButtonColor.color = PressedColor;
+        if (PresserCount == 1)
+        {
+            ConnectedDoor.OpenDoor();
+        }
     }
     private void OnCollisionExit2D(Collision2D _collision)
     {
-        MyTransform.position -= PressedPosition;
-        MyTransform.localScale -= PressedScale;
-        _collision.gameObject.transform.position -= PressedPosition * 2;
-        ConnectedDoor.CloseDoor();
+        PresserCount--;
+        if (PresserCount == 0)
+        {
+            ButtonColor.color = BaseColor;
+            ConnectedDoor.CloseDoor();
+        }
     }
 }
