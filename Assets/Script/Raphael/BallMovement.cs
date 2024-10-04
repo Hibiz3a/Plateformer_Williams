c7a53b6179609a07fe3bdc32e11a4b05c385eb21
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-10-04 10:31:21 +0200

    adjust difficulty

--- Assets/Script/Raphael/BallMovement.cs
+++ Assets/Script/Raphael/BallMovement.cs
@@ -8,6 +8,7 @@ public class BallMovement : MonoBehaviour
     private readonly float BallVelocity=5;
     private Rigidbody2D BallRB;
     Vector2 FwdDir;
+    [Range(1, 5)] private float pow=1;
     
 
     void Start()
@@ -20,14 +21,31 @@ public class BallMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            BallRB.AddForce(new Vector2(-1f, 0));
+            BallRB.simulated = true;
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (BallRB.simulated)
         {
-            BallRB.AddForce(new Vector2(1f, 0));
-        }       
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                BallRB.AddForce(new Vector2(-(pow), 0));
+                pow += Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                BallRB.AddForce(new Vector2((pow), 0));
+                pow += Time.deltaTime;
+            }
+            if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                pow = 1;
+            }
+            if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                pow = 1;
+            }
+        }     
     }
     private void FixedUpdate()
     {
@@ -39,6 +57,26 @@ public class BallMovement : MonoBehaviour
         {
             BallRB.velocity = new Vector2(BallRB.velocity.x, -3);
         }
+        else if(BallRB.velocity.y < -6)
+        {
+            BallRB.velocity = new Vector2(BallRB.velocity.x, -6);
+        }
+        else if (BallRB.velocity.y > 6)
+        {
+            BallRB.velocity = new Vector2(BallRB.velocity.x, 6);
+        }
+        if (BallRB.velocity.x > 6)
+        {
+            BallRB.velocity = new Vector2(6, BallRB.velocity.y);
+        }
+        else if (BallRB.velocity.x <-6)
+        {
+            BallRB.velocity = new Vector2(-6, BallRB.velocity.y);
+        }
+        if (BallRB.velocity.magnitude ==0)
+        {
+            BallRB.velocity = new Vector2(0,2);
+        }
     }
 
 
@@ -48,7 +86,7 @@ public class BallMovement : MonoBehaviour
         ContactPoint2D contact = _collision.GetContact(0);
         Vector3 newDirection = Vector2.Reflect(FwdDir, contact.normal);
         FwdDir =newDirection.normalized;
-        BallRB.AddForce(FwdDir * BallVelocity, ForceMode2D.Impulse);
+        BallRB.AddForce(Vector2.ClampMagnitude(FwdDir * BallVelocity,5), ForceMode2D.Impulse);
     }
     
     
