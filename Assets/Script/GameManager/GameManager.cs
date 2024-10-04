c71ccb8ad04753668ca101da3a8519aaa81b8f9c
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2024-10-04 12:27:22 +0200

    Add Level text

--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private CanvasGroup fadeInFadeOutCanvasGroup;
+    [SerializeField] private CanvasGroup ScoreAndTime;
 
     [SerializeField] private TextMeshProUGUI ScoreGUI;
     [SerializeField] private TextMeshProUGUI TimeGUI;
+    [SerializeField] private TextMeshProUGUI LevelGUI;
 
     private float Score = 0f;
     private float Timer = 0f;
     private float CurrentTimer = 0f;
+    private int CurrentScene = 0;
+    private bool NewLevel = false;
+    public static GameManager Instance;
 
     public float _score
     {
@@ -24,14 +29,13 @@ public class GameManager : MonoBehaviour
         get { return Timer; }
     }
 
-    public static GameManager Instance;
 
-    private int CurrentScene = 0;
 
     void Start()
     {
         ScoreGUI.text = "Score : " + Mathf.RoundToInt(Score);
         TimeGUI.text = "Time : " + CurrentTimer;
+        LevelGUI.text = "Niveau : " + 1;
 
         if (Instance == null)
         {
@@ -49,8 +53,11 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        CurrentTimer += Time.deltaTime;
-        TimeGUI.text = "Time : " + Mathf.RoundToInt(CurrentTimer);
+        if (NewLevel || CurrentScene == 0)
+        {
+            CurrentTimer += Time.deltaTime;
+            TimeGUI.text = "Time : " + Mathf.RoundToInt(CurrentTimer);
+        }
         //TimerToZero();
     }
 
@@ -62,34 +69,42 @@ public class GameManager : MonoBehaviour
         }
     }*/
 
-    public void NextLevel(float _levelTime, int _levelscore = 100)
+    public void NextLevel(float _levelTime, int _level, int _levelscore = 100)
     {
-        StartCoroutine(FadeInFadeOut());
         NormalizeScore(_levelscore, _levelTime);
-        CurrentTimer = Timer;
         ScoreGUI.text = "Score : " + Mathf.RoundToInt(Score);
+        CurrentTimer = Timer;
+        CurrentScene++;
+        StartCoroutine(FadeInFadeOut(_level));
     }
 
-    private IEnumerator FadeInFadeOut()
+    private IEnumerator FadeInFadeOut(int _level)
     {
         float alpha = 0;
+        float beta = 1;
         for (int i = 0; i < 10; i++)
         {
             alpha += 0.10f;
+            beta -= 0.10f;
             fadeInFadeOutCanvasGroup.alpha = alpha;
+            ScoreAndTime.alpha = beta;
             yield return new WaitForSeconds(0.05f);
         }
 
-        yield return new WaitForSeconds(1f);
-        CurrentScene++;
+        LevelGUI.text = "Niveau : " + _level;
+        yield return new WaitForSeconds(2f);
+        //CurrentScene++;
         End();
         SceneManager.LoadScene(CurrentScene);
         for (int i = 0; i < 10; i++)
         {
             alpha -= 0.10f;
+            beta += 0.10f;
             fadeInFadeOutCanvasGroup.alpha = alpha;
+            ScoreAndTime.alpha = beta;
             yield return new WaitForSeconds(0.05f);
         }
+        NewLevel = true;
     }
 
 
@@ -104,7 +119,6 @@ public class GameManager : MonoBehaviour
     private void NormalizeScore(int _levelScore, float _levelTime)
     {
         float _normalizedTimer = (_levelTime / CurrentTimer) + 1;
-
         Score = Score + (_levelScore * _normalizedTimer);
     }
 }
--- Assets/Script/GameManager/LevelEnd.cs
+++ Assets/Script/GameManager/LevelEnd.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class LevelEnd : MonoBehaviour
 {
     [SerializeField] private float LevelTime;
+    [SerializeField] private int Level;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.Instance.NextLevel(LevelTime);
+            GameManager.Instance.NextLevel(LevelTime, Level);
         }
     }
 }
