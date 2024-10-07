c7d5728b279fd9c030f5d8728ec3615fb6aa3a57
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-10-07 17:01:32 +0200

    level made easier

--- Assets/Script/Raphael/BallMovement.cs
+++ Assets/Script/Raphael/BallMovement.cs
@@ -32,31 +32,32 @@ public class BallMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
                 BallRB.AddForce(new Vector2(-(pow), 0));
-                pow += Time.deltaTime;
+                pow += Time.deltaTime*2;
             }
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 BallRB.AddForce(new Vector2((pow), 0));
-                pow += Time.deltaTime;
+                pow += Time.deltaTime*2;
             }
             if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
             {
-                pow = 1;
+                pow = 4;
             }
             if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
             {
-                pow = 1;
+                pow = 4;
             }
         }     
     }
     private void FixedUpdate()
     {
-        if (BallRB.velocity.y < 3 && BallRB.velocity.y>=0)
+        if (BallRB.velocity.x <= -5) 
         {
-            BallRB.velocity = new Vector2(BallRB.velocity.x, 3);
+            BallRB.velocity = new Vector2(-5, BallRB.velocity.y);
         }
-        else if(BallRB.velocity.y > -3 && BallRB.velocity.y < 0)
+        if (BallRB.velocity.x >= 5)
         {
+<<<<<<< Updated upstream
             BallRB.velocity = new Vector2(BallRB.velocity.x, -3);
         }
         else if(BallRB.velocity.y < -6)
@@ -82,18 +83,10 @@ public class BallMovement : MonoBehaviour
         if(Mathf.Abs(BallRB.velocity.x)<0.1f && Mathf.Abs(BallRB.velocity.y) < 0.1f)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y - 2, transform.position.z);
+=======
+            BallRB.velocity = new Vector2(5, BallRB.velocity.y);
+>>>>>>> Stashed changes
         }
     }
 
-
-
-    private void OnCollisionEnter2D(Collision2D _collision)
-    {
-        ContactPoint2D contact = _collision.GetContact(0);
-        Vector3 newDirection = Vector2.Reflect(FwdDir, contact.normal);
-        FwdDir =newDirection.normalized;
-        BallRB.AddForce(Vector2.ClampMagnitude(FwdDir * BallVelocity,5), ForceMode2D.Impulse);
-    }
-    
-    
 }
